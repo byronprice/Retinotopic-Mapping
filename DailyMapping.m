7035944f2c190340779856e25305a87ca160166d
byronprice <byron.h.price@gmail.com>
2016-08-02 20:11:10 -0400

    crontab fix plus regular expressions

--- DailyMapping.m
+++ DailyMapping.m
@@ -1,6 +1,6 @@
 %DailyMapping.m
-%   Used along with a shell script to automatically run all the animals
-%    from the current day, or all those whose data has not been analyzed
+%   Used along with a shell script to automatically map retinotopy for
+%    all the animals from the previous day
 %
 %INPUT: Will run through all of the files in the CloudStation folder
 %        ByronExp/RetinoExp and see if any new files exist, if they do, it
@@ -14,19 +14,22 @@
 
 cd('~/CloudStation/ByronExp/RetinoExp');
 
-today = datetime('today','Format','yyyy-MM-dd');
-today = char(today); today = strrep(today,'-','');
-today = str2double(today);
+yest = datetime('yesterday','Format','yyyy-MM-dd');
+yest = char(yest); yest = strrep(yest,'-','');
+yest = str2double(yest);
 
 fileStart = 'RetinoData*.plx';
 
 fileList = dir(fileStart);
 numFiles = size(fileList,1);
 
+datelen = 8;
+idlen = 5;
 for ii=1:numFiles
-    AnimalName = str2double(fileList(ii).name(end-8:end-4));
-    Date = str2double(fileList(ii).name(end-17:end-10));
-    if Date == today
+    index = regexp(fileList(ii).name,'_');
+    AnimalName = str2double(fileList(ii).name(index+1:index+idlen));
+    Date = str2double(fileList(ii).name(index-datelen:index-1));
+    if Date == yest
         [~,~,~] = MapRetinotopy(AnimalName,Date);
     end
 end
