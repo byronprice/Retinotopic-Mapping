4a16790e5713a58647f80868f941141f1f0eabfa
byronprice <byron.h.price@gmail.com>
2017-02-14 08:45:30 -0500

    Corrections to MCMC

--- BayesianFitLFPModel.m
+++ BayesianFitLFPModel.m
@@ -8,81 +8,46 @@ numParameters = 7;
    
 finalParameters = zeros(numChans,numParameters);
 
-% priorParams = [1.75,100;1000,500;700,400;1.75,300;1.75,300;1.75,200;1.75,150];
-proposal = [-150,100;1000,500;700,500;250,200;250,200;200,150;-200,150];
-
-Bounds = [-500,0;min(xaxis),max(xaxis);min(yaxis),max(yaxis);1,1000;1,1000;1,1000;-500,0];
+priorParams = [1.75,100;4,300;4,250;1.75,200;1.75,200;1.75,150;1.75,150];
+% proposal = [150,100;1000,500;700,500;300,200;250,300;200,150;200,150];
         
-N = 50000;
+N = 1000000;
 x = zeros(N,numParameters);
 
-for ii=1:numParameters
-   x(1,ii) = Bounds(ii,1)+(Bounds(ii,2)-Bounds(ii,1)).*rand;
+for jj=1:numParameters
+   x(1,jj) = gamrnd(priorParams(jj,1),priorParams(jj,2));
 end
 
-% for jj=[2,3]
-%    x(1,jj) = normrnd(priorParams(jj,1),priorParams(jj,2)); 
-% end
-% for jj=[4,5,6]
-%    x(1,jj) = gamrnd(priorParams(jj,1),priorParams(jj,2));
-% end
-% for jj=[1,7]
-%     x(1,jj) = -gamrnd(priorParams(jj,1),priorParams(jj,2));
-% end
 
 for zz=1:numChans
     randVals = rand([N,1]);
     flashPoints = centerVals(squeeze(Response(zz,:,1)),:);
     peakNegativity = squeeze(Response(zz,:,2));
     for ii=2:N
-%         priorXstar = 1;priorXprev = 1;qXstar = 1;qXprev = 1;
-%         for jj=[2,3]
-%             x(ii,jj) = normrnd(x(ii-1,jj),priorParams(jj,2));
-%             priorXstar = priorXstar*normpdf(x(ii,jj),priorParams(jj,1),priorParams(jj,2));
-%             priorXprev = priorXprev*normpdf(x(ii-1,jj),priorParams(jj,1),priorParams(jj,2));
-%             qXstar = qXstar*normpdf(x(ii-1,jj),x(ii,jj),priorParams(jj,2));
-%             qXprev = qXprev*normpdf(x(ii,jj),x(ii-1,jj),priorParams(jj,2));
+%         priorXstar = 1;priorXprev = 1;
+        xStar = x(ii-1,:)+normrnd(0,1,[1,numParameters]);
+%         check = 0;
+%         for jj=1:numParameters
+%             if xStar(jj) < 0
+%                 check = 1;
+%             end
+% %             priorXstar = priorXstar*gampdf(xStar(jj),priorParams(jj,1),priorParams(jj,2));
+% %             priorXprev = priorXprev*gampdf(x(ii-1,jj),priorParams(jj,1),priorParams(jj,2));
 %         end
-%         for jj=[4,5,6]
-%             x(ii,jj) = gamrnd(priorParams(jj,1),x(ii-1,jj)/priorParams(jj,1));
-%             priorXstar = priorXstar*gampdf(x(ii,jj),priorParams(jj,1),priorParams(jj,2));
-%             priorXprev = priorXprev*gampdf(x(ii-1,jj),priorParams(jj,1),priorParams(jj,2));
-%             qXstar = qXstar*gampdf(x(ii-1,jj),priorParams(jj,1),x(ii,jj)/priorParams(jj,1));
-%             qXprev = qXprev*gampdf(x(ii,jj),priorParams(jj,1),x(ii-1,jj)/priorParams(jj,1));
-%         end
-%         for jj=[1,7]
-%             x(ii,jj) = -gamrnd(priorParams(jj,1),-x(ii-1,jj)/priorParams(jj,1));
-%             priorXstar = priorXstar*gampdf(-x(ii,jj),priorParams(jj,1),priorParams(jj,2));
-%             priorXprev = priorXprev*gampdf(-x(ii-1,jj),priorParams(jj,1),priorParams(jj,2));
-%             qXstar = qXstar*gampdf(-x(ii-1,jj),priorParams(jj,1),-x(ii,jj)/priorParams(jj,1));
-%             qXprev = qXprev*gampdf(-x(ii,jj),priorParams(jj,1),-x(ii-1,jj)/priorParams(jj,1));
-%         end
-        priorXstar = 1;priorXprev = 1;qXstar = 1;qXprev = 1;
-        for jj=1:numParameters
-            x(ii,jj) = normrnd(x(ii-1,jj),proposal(jj,2));
-            if x(ii,jj) < Bounds(jj,1) || x(ii,jj) > Bounds(jj,2)
-                priorXstar = 0;
-            end
-            if x(ii-1,jj) < Bounds(jj,1) || x(ii-1,jj) > Bounds(jj,2)
-               priorXprev = 0; 
-            end
-            qXstar = qXstar*normpdf(x(ii-1,jj),x(ii,jj),proposal(jj,2));
-            qXprev = qXprev*normpdf(x(ii,jj),x(ii-1,jj),proposal(jj,2));
-        end
-        
-        likelihoodXstar = GetLikelihood(reps,x(ii,:),peakNegativity,flashPoints);
+
+        likelihoodXstar = GetLikelihood(reps,xStar,peakNegativity,flashPoints);
         likelihoodXprev = GetLikelihood(reps,x(ii-1,:),peakNegativity,flashPoints);
-        
-        A = min(1,(likelihoodXstar*priorXstar*qXstar)./(likelihoodXprev*priorXprev*qXprev));
-        
-        if randVals(ii) > A
+%         A = min(1,(likelihoodXstar*priorXstar)./(likelihoodXprev*priorXprev));
+        A = min(1,likelihoodXstar/likelihoodXprev);
+        if randVals(ii) > A || sum(xStar<0) > 0
             x(ii,:) = x(ii-1,:);
+        else
+            x(ii,:) = xStar;
         end
     end
     figure();
     for ii=1:numParameters
-       subplot(4,2,ii);histogram(x(5000:end,ii)); 
-       mode(x(5000:end,ii))
+       subplot(4,2,ii);histogram(x(100000:end,ii)); 
     end
 end
 
