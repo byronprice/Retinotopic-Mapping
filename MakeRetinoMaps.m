0705a837de9d58e2b4ffa1c9f9575839bc36d053
byronprice <byron.h.price@gmail.com>
2017-06-15 00:02:01 -0400

    Bayes posterior predictive maps

--- MakeRetinoMaps.m
+++ MakeRetinoMaps.m
@@ -5,52 +5,46 @@ function [h] = MakeRetinoMaps(AnimalName,Date)
 %
 %Created: 2017/02/22, 10 Lawrence Street, Cambridge
 %  Byron Price
-%Updated: 2017/03/12
+%Updated: 2017/06/14
 % By: Byron Price
 
-fileName = sprintf('RetinoMap%d_%d.mat',Date,AnimalName);
+fileName = sprintf('RetinoMapBayes%d_%d.mat',Date,AnimalName);
 load(fileName)
 
-numChans = size(dimReduceData,1);
-x = 1:w_pixels;y = 1:h_pixels;
+numChans = size(posteriorSample,1);
+x = 1:10:w_pixels;y = 1:10:h_pixels;
 
 for ii=1:numChans
     h(ii) = figure;
 end
-
+numSamples = size(posteriorSample,3);
 for ii=1:numChans
-    
     figure(h(ii));axis([0 w_pixels 0 h_pixels]);
-    title(sprintf('VEP Retinotopy: Chan %d, Animal %d',ii,AnimalName));
+    title(sprintf('LFP Retinotopy: Chan %d, Animal %d',ii,AnimalName));
     xlabel('Horizontal Screen Position (pixels)');ylabel('Vertical Screen Position (pixels)');
     hold on;
     
-    origVariance = var(dimReduceData{ii}(:,3));
     finalIm = zeros(length(x),length(y));
-    parameterVec = finalParameters(ii,:);
-    errors = ninetyfiveErrors(ii,:);
-    
-    fprintf('Baseline: %3.0f +/- %3.0f\n',parameterVec(6),errors(6));
-    fprintf('Rise: %3.0f +/- %3.0f\n',parameterVec(1),errors(1));
-    fprintf('Horizontal CoM: %3.0f +/- %3.0f mm\n',parameterVec(2)*mmPerPixel,errors(2)*mmPerPixel);
-    fprintf('Vertical CoM: %3.0f +/- %3.0f mm\n',parameterVec(3)*mmPerPixel,errors(3)*mmPerPixel);
-    fprintf('Horizontal spread: %3.0f +/- %3.0f mm\n',parameterVec(4)*mmPerPixel,errors(4)*mmPerPixel);
-    fprintf('Vertical spread: %3.0f +/- %3.0f mm\n',parameterVec(5)*mmPerPixel,errors(5)*mmPerPixel);
-  
-%     varianceExplained = (origVariance-parameterVec(7)^2)./origVariance;
-%     fprintf('Variance Explained: %3.1f %%\n\n',varianceExplained*100);
-    
-    for jj=1:length(x)
-        for kk=1:length(y)
-            distX = x(jj)-parameterVec(2);
-            distY = y(kk)-parameterVec(3);
-            b = [parameterVec(1),parameterVec(4),parameterVec(5),parameterVec(6)];
-            finalIm(jj,kk) = b(1)*exp(-(distX.^2)./(2*b(2)*b(2))-(distY.^2)./(2*b(3)*b(3)))+b(4);
+    samples = squeeze(posteriorSample(ii,:,:));
+    N = 1000;
+    for ll=1:N
+        index = random('Discrete Uniform',numSamples);
+        parameterVec = samples(:,index);
+        b = [parameterVec(1),parameterVec(4),parameterVec(5),parameterVec(6)];
+        for jj=1:length(x)
+            for kk=1:length(y)
+                distX = x(jj)-parameterVec(2);
+                distY = y(kk)-parameterVec(3);
+                
+                finalIm(jj,kk) = finalIm(jj,kk)+b(1)*exp(-(distX.^2)./(2*b(2)*b(2))-...
+                    (distY.^2)./(2*b(3)*b(3)))+b(4);
+            end
         end
     end
+    finalIm = finalIm./N;
     imagesc(x,y,finalIm');set(gca,'YDir','normal');w=colorbar;
-    caxis([b(4) b(4)+100]);
-    ylabel(w,'Mean VEP Magnitude (\muV)');colormap('jet');hold off;
+    caxis([b(4) b(4)+150]);
+    ylabel(w,'Log Mean VEP Magnitude (\muV)');colormap('jet');hold off;
     
 end
 
