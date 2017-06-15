123c82384d5a1ffb34d7a6c483840e4ff528166b
byronprice <byron.h.price@gmail.com>
2017-06-15 10:55:40 -0400

    Randomized starts to find optimum

--- FitLFPRetinoModel_Bayes.m
+++ FitLFPRetinoModel_Bayes.m
@@ -67,7 +67,7 @@ for zz=1:numChans
     paramSet = zeros(numParameters,numStarts);
     for jj=1:numStarts
         
-        for ii=[4,5,6,7]
+        for ii=4:7
             paramSet(ii,jj) = gamrnd(priorParams(ii,1),priorParams(ii,2));
         end
         paramSet(1,jj) = gamrnd(9.1,25.09); 
@@ -83,38 +83,86 @@ for zz=1:numChans
                sum(log(normpdf(paramSet(2:3,jj),priorParams(2:3,1),priorParams(2:3,2))))+...
                log(exppdf(paramSet(1,jj),priorParams(1,2)));
         maxPost(jj) = loglikelihood+logPrior;
+        
+        
+        updateParam = 0.1;
+        mu = zeros(numParameters,1);sigma = eye(numParameters);
+        for ii=1:numParameters
+            if ii<=3
+                sigma(ii,ii) = priorParams(ii,2)^2/2;
+            else
+                sigma(ii,ii) = priorParams(ii,1)*(priorParams(ii,2)^2)/2;
+            end
+        end
+%         halfSigma = cholcov(sigma);identity = eye(numParameters);
+        loglambda = log(2.38^2/numParameters);
+        updateMu = zeros(numParameters,1);
+        
+        for ii=4:7
+            updateMu(ii,1) = gamrnd(priorParams(ii,1),priorParams(ii,2));
+        end
+        updateMu(1,1) = gamrnd(9.1,25.09);
+        updateMu(2,1) = normrnd(priorParams(2,1),priorParams(2,2));
+        updateMu(3,1) = normrnd(priorParams(3,1),priorParams(3,2));
+        updateMu = min(max(updateMu,lBound),uBound);
+        optimalAccept = 0.234;%0.44
+        %     scatter(1,posteriorProb(1));hold on;pause(0.5);
+        
+        for ii=1:100
+            pStar = paramSet(:,jj)+mvnrnd(mu,exp(loglambda)*sigma)';
+            
+            if sum(pStar<lBound) == 0 & sum(pStar>=uBound) == 0
+                inputParams = pStar;inputParams(7) = 1/inputParams(7);
+                pStarLogLikelihood = GetLikelihood(reps,inputParams,vepMagnitude,flashPoints);
+                pStarLogPrior = sum(log(gampdf(pStar(4:7),priorParams(4:7,1),priorParams(4:7,2))))+...
+                    sum(log(normpdf(pStar(2:3),priorParams(2:3,1),priorParams(2:3,2))))+...
+                    log(exppdf(pStar(1),priorParams(1,2)));
+                
+                logA = (pStarLogLikelihood+pStarLogPrior)-maxPost(jj);
+                
+                if logA>0
+                    paramSet(:,jj) = pStar;
+                    maxPost(jj) = pStarLogLikelihood+pStarLogPrior;
+                end
+                
+                meanSubtract = paramSet(:,jj)-updateMu;
+                updateMu = updateMu+updateParam.*meanSubtract;
+                sigma = sigma+updateParam.*(meanSubtract*meanSubtract'-sigma);
+                loglambda = loglambda+updateParam.*(exp(min(0,logA))-optimalAccept);
+                
+            else
+                loglambda = loglambda+updateParam.*(-optimalAccept);
+            end
+            %         error = abs(1394-updateMu(2))+abs(419-updateMu(3));
+%                     scatter(ii,maxPost(jj));%scatter(ii,error);
+%                     hold on;pause(0.01);
+        end
     end
     [~,ind] = max(maxPost);
     posteriorProb(1) = maxPost(ind);
     params(:,1) = paramSet(:,ind);
 
-    % FOR AUTOMATIC CREATION OF UPDATE MATRIX
-    updateParam = linspace(0.1,1e-2,burnIn);
+    % FOR AUTOMATIC CREATION OF PROPOSAL DISTRIBUTION
+    updateParam = 1e-2;
     mu = zeros(numParameters,1);sigma = eye(numParameters);
     for ii=1:numParameters
        if ii<=3
-           sigma(ii,ii) = priorParams(ii,2)^2;
+           sigma(ii,ii) = priorParams(ii,2)^2/2;
        else
-           sigma(ii,ii) = priorParams(ii,1)*(priorParams(ii,2)^2);
+           sigma(ii,ii) = priorParams(ii,1)*(priorParams(ii,2)^2)/2;
        end
     end
+    halfSigma = cholcov(sigma);identity = eye(numParameters);
     loglambda = log(2.38^2/numParameters);
-    updateMu = zeros(numParameters,1);
-    
-    for ii=[4,5,6,7]
-        updateMu(ii,1) = gamrnd(priorParams(ii,1),priorParams(ii,2));
-    end
-    updateMu(1,1) = gamrnd(9.1,25.09);
-    updateMu(2,1) = normrnd(priorParams(2,1),priorParams(2,2));
-    updateMu(3,1) = normrnd(priorParams(3,1),priorParams(3,2));
+    updateMu = params(:,1)+mvnrnd(mu,exp(loglambda)*sigma)';
+    updateMu = min(max(updateMu,lBound),uBound);
     optimalAccept = 0.234;%0.44
 %     scatter(1,posteriorProb(1));hold on;pause(0.5);
 
     for ii=2:burnIn
-        Z = mvnrnd(mu,exp(loglambda).*sigma)';
-        pStar = params(:,ii-1)+Z;
+        pStar = params(:,ii-1)+mvnrnd(mu,exp(loglambda)*sigma)';
         
-        if sum(pStar<=lBound) == 0 & sum(pStar>=uBound) == 0
+        if sum(pStar<lBound) == 0 & sum(pStar>=uBound) == 0
             inputParams = pStar;inputParams(7) = 1/inputParams(7);
             pStarLogLikelihood = GetLikelihood(reps,inputParams,vepMagnitude,flashPoints);
             pStarLogPrior = sum(log(gampdf(pStar(4:7),priorParams(4:7,1),priorParams(4:7,2))))+...
@@ -137,59 +185,66 @@ for zz=1:numChans
                 posteriorProb(ii) = posteriorProb(ii-1);
             end
             
-            
+            if mod(ii,100)==0
                 meanSubtract = params(:,ii)-updateMu;
-                updateMu = updateMu+updateParam(ii).*meanSubtract;
-                %             halfSigma = halfSigma+updateParam.*(triu((halfSigma^-1)*(halfSigma'*halfSigma+meanSubtract*...
-                %             meanSubtract')*((halfSigma^-1)')-identity)-halfSigma);
-                %             sigma = halfSigma'*halfSigma;
-                sigma = sigma+updateParam(ii).*(meanSubtract*meanSubtract'-sigma);
-                loglambda = loglambda+updateParam(ii).*(exp(min(0,logA))-optimalAccept);
+                updateMu = updateMu+updateParam.*meanSubtract;
+                halfSigma = halfSigma+updateParam.*(triu((inv(halfSigma))*(halfSigma'*halfSigma+meanSubtract*...
+                    meanSubtract')*((inv(halfSigma))')-identity)-halfSigma);
+                
+                sigma = halfSigma'*halfSigma;
+                %                 sigma = sigma+updateParam(ii).*(meanSubtract*meanSubtract'-sigma);
+                loglambda = loglambda+updateParam.*(exp(min(0,logA))-optimalAccept);
+            end
         else
             params(:,ii) = params(:,ii-1);
             posteriorProb(ii) = posteriorProb(ii-1);
-            loglambda = loglambda+updateParam(ii).*(-optimalAccept);
+            if mod(ii,100)==0
+                loglambda = loglambda+updateParam.*(-optimalAccept);
+            end
         end
 %         error = abs(1394-updateMu(2))+abs(419-updateMu(3));
 %         scatter(ii,posteriorProb(ii));%scatter(ii,error);
 %         hold on;pause(0.01);
     end
-   
-    [V,D] = eig(cov(params(:,5e4:50:burnIn)'));
-    W = V*sqrtm(D);
-    eigenvals = diag(W'*W);
     
-    
-    tempW = [];
-    tempEigs = [];
-    for jj=1:numParameters
-        if eigenvals(jj) > 1e-5
-            tempW = [tempW,W(:,jj)];
-            tempEigs = [tempEigs,eigenvals(jj)];
-        end
-    end
-    
-    q = length(tempEigs);
-    if q <= 2
-        [V,D] = eig(sigma);
-        W = fliplr(V*sqrtm(D));
-        eigenvals = fliplr(diag(W'*W));
-        q = length(eigenvals);
-    else
-        W = fliplr(tempW);
-        eigenvals = fliplr(tempEigs);
-        q = length(eigenvals);
-    end
-    p = ones(q,1)./q;
-    loglambda = loglambda.*ones(q,1);
-    updateParam = 1e-2;
-    
+%     [V,D] = eig(cov(params(:,1e4:100:burnIn)'));
+%     W = V*sqrtm(D);
+%     eigenvals = diag(W'*W);
+%     
+%     
+%     tempW = [];
+%     tempEigs = [];
+%     for jj=1:numParameters
+%         if eigenvals(jj) > 1e-5
+%             tempW = [tempW,W(:,jj)];
+%             tempEigs = [tempEigs,eigenvals(jj)];
+%         end
+%     end
+%     
+%     q = length(tempEigs);
+%     if q <= 2
+%         [V,D] = eig(sigma);
+%         W = fliplr(V*sqrtm(D));
+%         eigenvals = fliplr(diag(W'*W));
+%         q = length(eigenvals);
+%         fprintf('Error\n');
+%     else
+%         W = fliplr(tempW);
+%         eigenvals = fliplr(tempEigs);
+%         q = length(eigenvals);
+%     end
+% %     p = ones(q,1)./q;
+%     loglambda = loglambda.*ones(q,1);
+%     updateParam = 1e-3;
+    sigma = cov(params(:,1e4:100:burnIn)');
+    lambda = 2.38^2/numParameters;
     acceptRate = 0;
     for ii=burnIn+1:numIter
-        index = find(mnrnd(1,p)==1);
-        lambda = loglambda(index);
-        stdev = sqrt(exp(lambda).*eigenvals(index));
-        pStar = params(:,ii-1)+W(:,index)*normrnd(0,stdev);
+%         index = random('Discrete Uniform',q);
+%         lambda = loglambda(index);
+%         stdev = sqrt(exp(lambda).*eigenvals(index));
+%         pStar = params(:,ii-1)+W(:,index)*normrnd(0,stdev);
+        pStar = params(:,ii-1)+mvnrnd(mu,lambda*sigma)';
         
         if sum(pStar<lBound) == 0 & sum(pStar>=uBound) == 0
             inputParams = pStar;inputParams(7) = 1/inputParams(7);
@@ -213,13 +268,10 @@ for zz=1:numChans
                 params(:,ii) = params(:,ii-1);
                 posteriorProb(ii) = posteriorProb(ii-1);
             end
-            lambda = lambda+updateParam.*(exp(min(0,logA))-optimalAccept);
         else
             params(:,ii) = params(:,ii-1);
             posteriorProb(ii) = posteriorProb(ii-1);
-            lambda = lambda+updateParam.*(-optimalAccept);
         end
-        loglambda(index) = lambda;
     end
     params(7,:) = 1./params(7,:);
 %     params(8,:) = 1./exp(params(8,:));
