61053df252fd2ba8b441161e1cf315ea04bde60b
Byron Price <byron.h.price@gmail.com>
2017-10-22 17:50:24 -0400

    short-time FFT at stimulation frequency

--- MapRetinotopyCallaway.m
+++ MapRetinotopyCallaway.m
@@ -81,35 +81,119 @@ strobeTimes = tsevs{1,strobeStart};
 stimLen = round(driftTime*sampleFreq);
 
 % COLLECT DATA IN THE PRESENCE OF VISUAL STIMULI
-Response = cell(numChans,numDirs);
+Response = cell(numChans,3);
 
 for ii=1:numChans
+    Response{ii,1} = zeros(2*reps,stimLen);
+    Response{ii,2} = zeros(2*reps,stimLen);
+    
+    greyStrobes = strobeTimes(svStrobed==0);
+    numGrey = length(greyStrobes);
+    Response{ii,3} = zeros(2*numGrey,stimLen);
+    
+    count = 1;
+    for jj=1:numGrey
+       onsetTime = round(greyStrobes(jj)*sampleFreq); 
+       offsetTime = onsetTime+stimLen-1;
+       Response{ii,3}(count,:) = ChanData(onsetTime:offsetTime,ii);
+       count = count+1;
+       onsetTime = offsetTime+1;
+       offsetTime = onsetTime+stimLen-1;
+       Response{ii,3}(count,:) = ChanData(onsetTime:offsetTime,ii);
+       count = count+1;
+    end
+    
+    horzCount = 1;
+    vertCount = 1;
     for jj=1:numDirs
         strobeNum = jj;
         currentStrobeTimes = strobeTimes(svStrobed==strobeNum);
-        Response{ii,jj} = zeros(reps,stimLen);
         for kk=1:reps
            onsetTime = round(currentStrobeTimes(kk)*sampleFreq);
            offsetTime = onsetTime+stimLen-1;
-           Response{ii,jj}(kk,:) = ChanData(onsetTime:offsetTime,ii);
-        end
-        figure();
-        count = 1;
-        for ll=1:reps
-            for mm=1:20
-                x = Response{ii,jj}(ll,1+(mm-1)*500:500+(mm-1)*500);
-                y = fft(x);
-                y = sqrt(y.*conj(y));
-                f = linspace(0,sampleFreq/2,length(y)/2);
-                y = y(1:length(y)/2);
-                [~,ind] = min(abs(f-stimulationFrequency));
-%                 [~,newInd] = max(y(ind-2:ind+2));
-                subplot(reps,20,count);plot(f(ind-5:ind+5),y(ind-5:ind+5));axis([f(ind-5) f(ind+5) 0 5e4]);
-                count = count+1;
-            end
+           if strcmp(DirNames{jj},'Left') == 1 || strcmp(DirNames{jj},'Down') == 1
+              tempLFP = flipud(ChanData(onsetTime:offsetTime,ii));
+           else
+              tempLFP = ChanData(onsetTime:offsetTime,ii);
+           end
+           
+           if strcmp(DirNames{jj},'Left') == 1 || strcmp(DirNames{jj},'Right') == 1
+                Response{ii,1}(horzCount,:) = tempLFP;
+                horzCount = horzCount+1;
+           elseif strcmp(DirNames{jj},'Down') == 1 || strcmp(DirNames{jj},'Up') == 1
+                Response{ii,2}(vertCount,:) = tempLFP;
+                vertCount = vertCount+1;
+           end
         end
     end
 end
+
+time = linspace(0,driftTime,stimLen);
+horzPosition = linspace(0,w_pixels,stimLen); %w_pixels
+vertPosition = linspace(0,h_pixels,stimLen); %h_pixels
+
+waveletSize = 8;
+x = linspace(-waveletSize/2*checkRefresh,waveletSize/2*checkRefresh,round(waveletSize*checkRefresh*sampleFreq));
+kernel = exp(-2*pi*x*1i*stimulationFrequency);
+transformBaseline = zeros(numChans,1);
+for ii=1:numChans
+    numGrey = size(Response{ii,3},1);
+    temp = zeros(numGrey,stimLen);
+    for jj=1:numGrey
+        data = Response{ii,3}(jj,:);
+        data = conv(data,kernel,'same');
+        data = sqrt(data.*conj(data));
+        temp(jj,:) = data;
+    end
+    transformBaseline(ii) = mean(temp(:));
+end
+
+transformResponse = cell(numChans,2);
+for ii=1:numChans
+    for jj=1:2
+        transformResponse{ii,jj} = zeros(2*reps,stimLen);
+        for kk=1:2*reps
+            data = Response{ii,jj}(kk,:);
+            data = conv(data,kernel,'same');
+            data = sqrt(data.*conj(data));
+            transformResponse{ii,jj}(kk,:) = data-transformBaseline(ii);
+        end
+        y = transformResponse{ii,jj};y=y(:);
+        if jj==1
+            position = horzPosition;
+            position = repmat(position,[2*reps,1]);
+            
+        elseif jj==2
+            position = vertPosition;
+            position = repmat(position,[2*reps,1]);
+        end
+        design = [position(:),position(:).*position(:)];
+        [b,dev,stats] = glmfit(design,y,'normal','link','log');
+
+        [yhat,dylo,dyhi] = glmval(b,[position(1,:)',position(1,:)'.*position(1,:)'],'log',stats);
+        figure();plot(position(:),y,'.');hold on;
+        yhat = real(yhat);dylo = real(dylo);dyhi = real(dyhi);
+        boundedline(position(1,:)',yhat,[dylo,dyhi],'c');
+        title(sprintf('Chan: %d, Dir: %d',ii,jj));
+    end
+end
+
+% timebandwidth = 60; % approximate standard deviation in time is 
+%                 % 0.5*sqrt(timebandwidth/2)
+% for ii=1:numChans
+%     for jj=1:2
+%         figure();
+%         for kk=1:2*reps
+%             [wt,f] = cwt(Response{ii,jj}(kk,:),sampleFreq,'TimeBandwidth',timebandwidth);
+%             wt = sqrt(wt.*conj(wt));
+%             [~,ind] = min(abs(f-stimulationFrequency));
+%             wt = wt(ind:ind,:);
+%             wt = mean(wt,1);
+%             baseline = quantile(wt,0.05);
+%             subplot(2*reps,1,kk);plot(time,wt-baseline);
+%         end
+%     end
+% end
 
 % discrete or continuous wavelet transform, hilbert transform
 %  hilbert(x), dwt, or cwt
@@ -118,30 +202,11 @@ end
 % z = Response{1,1}(1,:);
 % w = conv(z,y);
 % plot(w.*w);
-
-% create sliding window spectrogram for power at the stimulation frequency as function
-%  of position on the screen
-fftSpectrogram = cell(numChans,numDirs);
-
-windowLen = floor(5*checkRefresh*sampleFreq);
-nOverlap = floor(windowLen/5);
-nfft = max(256,2^nextpow2(windowLen));
-for ii=1:numChans
-    for jj=1:numDirs
-        result = [];
-        for kk=1:reps
-            data = Response{ii,jj}(kk,:)';
-            [s,f,t] = spectrogram(data,windowLen,nOverlap,nfft,sampleFreq);
-            [~,ind] = min(abs(f-stimulationFrequency));
-            tempfft = s(ind-1:ind+1,:);
-            power = sqrt(tempfft.*conj(tempfft));
-            result = [result;power];
-        end
-        fftSpectrogram{ii,jj} = result;
-        confInt = 2*std(result,[],1)/sqrt(reps);
-        figure();boundedline(t',mean(result,1)',confInt');
-        title(sprintf('Chan: %d, Direction: %s',ii,DirNames{jj}));
-    end
 end
-pause(1);
+
+function [b,dev,se] = GetMLest(design,y,b)
+
+
+
+
 end
